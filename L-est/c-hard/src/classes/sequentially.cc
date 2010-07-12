e8769999859137f997e5d54c41a378c3cae4fbd2
Joseph Abrahamson <abrahamson.j@gmail.com>
2010-07-11 21:47:05 -0400

    The beginnings of `Sequentially`

---
+++ L-est/c-hard/src/classes/sequentially.cc
@@ -0,0 +1,28 @@
+#include "sequentially.hpp"
+
+#include <vector>
+
+Sequentially::Sequentially (samp_vec samplers_in) {
+  samplers = samplers_in;
+  S = samplers.size();
+}
+
+Sequentially::~Sequentially () {
+  // will call of the sampler's deconstructors as well.
+  delete &samplers;
+}
+
+Sequentially *
+Sequentially::clone () {
+  samp_vec samplers_new = samplers;
+  Sequentially *samp = new Sequentially(samplers_new);
+  return samp;
+}
+
+void
+Sequentially::jump (State *x) {
+  samp_vec::iterator it;  
+  for (it = samplers.begin() ; it < samplers.end(); it++) {
+    (*it)->jump(x);
+  }
+}
---
+++ L-est/c-hard/src/classes/sequentially.hpp
@@ -0,0 +1,46 @@
+#ifndef SEQUENTIALLY_H
+#define SEQUENTIALLY_H
+
+#include <vector>
+
+#include <classes/sampler.hpp>
+
+/** Updates the state by running through a collection of samplers
+    sequentially. */
+
+typedef std::vector <Sampler *> samp_vec;
+
+class Sequentially : public Sampler {
+
+  /// The vector of constituent samplers.
+  samp_vec samplers;
+  
+  /// The number of constituent samplers.
+  samp_vec::size_type S;
+
+  /// Initialize the Sequentially sampler with a std::vector of
+  /// Samplers. The Sequentially sampler takes ownership of each of
+  /// its constituents and will free them upon deletion.
+  Sequentially (samp_vec samplers_in);
+  
+  /// Frees each constituent sampler.
+  ~Sequentially ();
+  
+  /// Clone each constituent sampler to form a new Sequentially
+  /// sampler.
+  Sequentially *clone ();
+
+  /// Running through the samplers vector, jump the state in each
+  /// sampler then return.
+  void jump (State *);
+
+  /// The union of the `isFrozen` states of each constituent sampler.
+  bool isFrozen ();
+
+  /// Initialize the state by using the each constituent sampler
+  /// sequentially. Overridden since Sequentially does not have its
+  /// own `view`, `update`, or `guess0` functions.
+  void initState (State *st);
+};
+
+#endif
